120582c1257008236b0a195cf85556bf2a07700c
Brad Gessler <bradgessler@gmail.com>
2023-02-15 12:54:41 -0800

    Copy code from gist into project

--- lib/rowdy.rb
+++ lib/rowdy.rb
@@ -4,5 +4,66 @@ require_relative "rowdy/version"
 
 module Rowdy
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Router
+    def routes
+      @routes ||= []
+    end
+
+    def add(*args)
+      routes.append args
+    end
+  end
+
+  class Dispatcher
+    def initialize(application:, router: nil)
+      @application = application
+      @router = router || application.router
+      p [:routes, @router.routes]
+    end
+
+    def call(path:, method: :get, params: {})
+      p requested_route = [ method.to_sym, path.to_sym ]
+      p case @router.routes.find { |route| route == requested_route }
+        in [_, action_name]
+          action = @application.method(action_name)
+          response = action.arity.zero? ? action.call : action.call(**params)
+          [ 200, response ]
+        in nil
+          [ 404, "Not Found" ]
+      end
+    end
+  end
+
+  module Routing
+    HTTP_METHODS = %i[get put patch post delete]
+
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    def router
+      self.class.router
+    end
+
+    def dispatcher
+      Dispatcher.new(application: self)
+    end
+
+    def call(...)
+      dispatcher.call(...)
+    end
+
+    module ClassMethods
+      HTTP_METHODS.each do |http_method|
+        define_method http_method do |action|
+          router.add http_method, action
+        end
+      end
+
+      def router
+        @router ||= Router.new
+      end
+    end
+  end
 end
--- spec/rowdy_spec.rb
+++ spec/rowdy_spec.rb
@@ -6,6 +6,101 @@ RSpec.describe Rowdy do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    class Application
+      include Rowdy::Routing
+
+      get def welcome
+        "hi"
+      end
+
+      get def greet(greeting)
+        "Hello #{greeting}"
+      end
+
+      get def show(id:)
+        "Finding #{id}"
+      end
+
+      post def create(title:, description:)
+        "Creating #{title} #{description}"
+      end
+
+      patch def bulk(ids: [])
+        "Do some bulk stuff with all these ids: #{ids.inspect}"
+      end
+    end
+
+    class SubApplication < Application
+      def show(id:)
+        "This is the sub-app ... #{id}"
+      end
+    end
+
+    class Person < Data.define(:name, :age, :id)
+      def self.find(id)
+        new id: id, age: nil, name: nil
+      end
+    end
+
+    class Animal < Data.define(:name, :age, :id)
+      def self.find(id)
+        new id: id, age: nil, name: nil
+      end
+    end
+
+    class Resource
+      include Rowdy::Routing
+
+      def initialize(model:)
+        @model = model
+      end
+
+      get def show(id:) = "Finding #{@model.find(id).inspect}"
+      post def create(**kwargs) = "Creating #{@model.new(**kwargs)}"
+
+      patch def bulk(ids: [])
+        models = ids.map { |id| @model.find id }
+        "Do some bulk stuff with all these models: #{models.inspect}"
+      end
+    end
+
+    ### This would be handled by the web server ... dev doesn't see it.
+
+    app = Application.new
+    app.call(path: "welcome")
+    app.call(path: "create", method: :post, params: {title: "Hi", description: "There"})
+    begin
+      app.call(path: "create", method: :post, params: {title: "Hi"})
+    rescue ArgumentError
+      puts "This would return an HTTP code that the request is invalid"
+    end
+    app.call(path: "show", params: {id: 1})
+    app.call(path: "create", method: :post, params: {title: "Hi", description: "There"})
+    app.call(path: "bulk", method: :patch, params: {ids: (1..10).to_a})
+
+    # This doesn't inherit the routes yet 😢
+    sub_app = SubApplication.new
+    sub_app.call(path: "show", params: {id: 1})
+
+
+    person_resource = Resource.new(model: Person)
+    person_resource.call(path: "show", params: {id: 1})
+    person_resource.call(path: "bulk", method: :patch, params: {ids: [1,3,7]})
+
+    animal_resource = Resource.new(model: Animal)
+    animal_resource.call(path: "show", params: {id: 11})
+    animal_resource.call(path: "bulk", method: :patch, params: {ids: [11,33,77]})
+
+    ### Next thing to solve would be mounting apps inside of apps ... so something like this is possible...
+
+    # class Application
+    #   mount Resource.new(model: Person),
+    #   mount Resource.new(model: Animal), at: "/animals"
+    #
+    #   get def greet
+    #     "Hello! Check out /persons and /animals"
+    #   end
+    # end
+
   end
 end
